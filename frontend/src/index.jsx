7cb0ea582400fb0ca644e5d43b65ba99d0a8e3d4
Kaspatou <54689116+Kaspatou07@users.noreply.github.com>
2025-12-17 01:58:37 +0100

    Update index.jsx (redirect)

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles/index.css'; 
+import './styles/index.css';
+
+const redirect = sessionStorage.redirect;
+if (redirect) {
+  sessionStorage.removeItem("redirect");
+  window.history.replaceState(null, "", redirect);
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
